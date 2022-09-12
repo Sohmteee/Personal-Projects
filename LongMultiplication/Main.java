c90a837f3f99cb09b443c6f359628f0aab2e6eff
Sohmteee <sohmtee@gmail.com>
2022-09-13 00:48:24 +0100

    commit

--- LongMultiplication/Main.java
+++ LongMultiplication/Main.java
@@ -17,8 +17,8 @@ public class Main {
             enterAEnterB();
 
             // to prevent a few bugs, we need to restrict the range of numbers that can be calculated
-            if ((c > 2_000_000_000)) {
-                while ((c > 2_000_000_000) | (c < 0)) {
+            if ((c > 2_000_000_000) || (String.valueOf(c).length() > 9)) {
+                while ((c > 2_000_000_000) || (c > 2_000_000_000) | (c < 0)) {
                     System.out.println("\nNumbers are too big (product is greater than 2,000,000,000)");
                     enterAEnterB();
                 }
